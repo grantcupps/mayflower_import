f4fb0900b963419cb2746cdbdfa85e3e2161cf77
Grant Cupps <grant.cupps@gmail.com>
2014-07-23 20:57:57 -0500

    V1 of the script

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ mayflower_import
 ================
 
 A script that automatically find's the Yelp ID for restaurants on a CSV file.
+
+
+# Use
+* Clone or download the repo
+* Run ``` bundle install `` from inside the directory
+* Add your Yelp API keys
+* Run the script ``` ruby import.rb file.csv ``` where file.csv is the original file.
+
+# Items of note
+* The script assumes there isn't a header, the business is in the first column and the city is in the second column
--- import.rb
+++ import.rb
@@ -2,3 +2,37 @@
 
 require "csv"
 require "yelpster"
+
+file = ARGV[0]
+
+abort "Error: Must include CSV file" if file.nil?
+
+Yelp.configure(
+   :consumer_key    => '',
+   :consumer_secret => '',
+   :token           => '',
+   :token_secret    => ''
+)
+
+# construct a client instance
+client = Yelp::Client.new
+
+include Yelp::V2::Search::Request
+
+businesses = CSV.read(file, {})
+
+businesses.each do |row|
+  request = Location.new(
+              :term => row[0],
+              :city => row[1],
+              :limit => "1")
+  response = client.search(request)
+
+  row << response["businesses"].first["id"]
+end
+
+CSV.open("results.csv", "w") do |csv|
+  businesses.each do |row|
+    csv << row
+  end
+end
